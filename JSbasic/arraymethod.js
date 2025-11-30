cb0b9b7240b534193182fe637881a7303b1ec5a6
priyanshu-6264 <priyanshulowanshi52@gmail.com>
2025-11-30 15:19:19 +0530

    first commit

---
+++ JSbasic/arraymethod.js
@@ -0,0 +1,62 @@
+//Push method 
+/*let foodItems = ["pateto","apple","litchi","tomato"];
+foodItems.push("chips","paneer");
+console.log(foodItems);*/
+
+
+//Pop method 
+/*let foodItems = ["pateto","apple","litchi","tomato"];
+foodItems.pop();
+console.log(foodItems);*/
+
+//toString method
+/*let foodItems = ["pateto","apple","litchi","tomato"];
+console.log(foodItems);
+console.log(foodItems.toString());
+console.log(foodItems);*/
+
+//concat method 
+
+/*let marvel_heros = ["thor","spidermen","ironman"];
+let dcHeros = ["superman","betman"];
+let heroes = marvel_heros.concat(dcHeros);
+console.log(heroes);*/
+
+//unshift method 
+/*let marvel_heros = ["thor","spidermen","ironman"];
+marvel_heros.unshift("antman");
+console.log(marvel_heros);*/
+
+//shift method 
+/*let marvel_heros = ["thor","spidermen","ironman"];
+marvel_heros.shift("thor");
+console.log(marvel_heros);*/
+
+// slice method 
+/*let marvel_heros = ["thor","spidermen","ironman","antman"];
+console.log(marvel_heros);
+console.log(marvel_heros.slice(1,3));*/
+
+// splice method 
+/*let arr = [1,2,3,4,5,6,7];
+
+//Add Element
+//arr.splice(2,0,101);
+
+//Delete Element
+//arr.splice(3,1);
+
+//Replace Element
+//arr.splice(3,1,101);
+
+console.log(arr);*/
+
+
+//Prectice Question
+let companies =["Blooomberge", "Microsoft","Uber", "Google","IBM","Netflix"];
+//companies.shift();
+//companies.splice(2,1,"ola");
+companies.push("Amazon");
+console.log(companies);
+
+
